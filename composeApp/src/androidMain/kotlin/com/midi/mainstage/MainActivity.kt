296eb6ce7f745cca34ba176b26f88e732fc38342
Mateo Joel Lopez <thefilesystem1024@gmail.com>
2026-08-26 22:56:43 -0500

    Enable Full Screen Immersive Mode in MainActivity

--- composeApp/src/androidMain/kotlin/com/midi/mainstage/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/midi/mainstage/MainActivity.kt
@@ -5,6 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 private const val TAG = "StageKeysMain"
 
@@ -14,6 +17,12 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        
+        // Hide system bars (Full Screen Immersive Mode)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.systemBars())
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
 
         // Initialize Android base folder for file persistence
         setAndroidBaseDir(filesDir)
